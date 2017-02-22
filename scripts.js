cff246e29e46b29dfc16cc2309ffcc7ba2e3ffea
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-02-21 19:04:38 -0600

    adding button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 console.log("hello");
+var output = document.getElementById("planet-holder");
+var button = document.getElementById("show-planets");
+
+button.addEventListener("click", addText);
+
+function addText(){
+  output.innerHTML = "This is a string";
+}
